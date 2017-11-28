bb8f213051e2b04ef0994c7f8320c4a00eceab33
marc-e3 <me236@njit.edu>
2017-11-28 11:45:05 -0500

    PowerUp
    Added PowerUp to all levels

--- Assets/Scripts/AsteroidSpawn.cs
+++ Assets/Scripts/AsteroidSpawn.cs
@@ -6,20 +6,25 @@ using UnityEngine;
 public class AsteroidSpawn : MonoBehaviour {
     // Spawning asteroids
     public GameObject Asteroid1;
+    public GameObject PowerUp;
     //Teleport teleport;
     //int asteroidsleft;
     public int AsteroidCount;
     public float AsteroidTime;
+    public float PowerTime;
     public float firstAsteroid;
+    public float firstPower;
     public Vector3 size;
     public Vector3 center;
     public GameObject AsteroidPrefab;
+    public GameObject PowerUpContainer;
     // Use this for initialization
     public Text progress;
 
     void Start()
     {
         StartCoroutine(SpawnAsteroid1());
+        StartCoroutine(SpawnPowerup());
 
         //AsteroidsLeft = AsteroidCount;  //setting the amount of asteroids left to destroy to the amount it originally started with
         //asteroidsleft = teleport.counter;
@@ -43,6 +48,18 @@ public class AsteroidSpawn : MonoBehaviour {
            Instantiate(AsteroidPrefab, pos, spawnRotation);
            yield return new WaitForSeconds(AsteroidTime);
       }
+    }
+    IEnumerator SpawnPowerup()
+    {
+        yield return new WaitForSeconds(firstPower);
+
+        for (int i = 1; i <= 3; i++)
+        {
+            Vector3 pos = center + new Vector3(Random.Range(-size.x / 2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z / 2, size.z / 2));
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(PowerUpContainer, pos, spawnRotation);
+            yield return new WaitForSeconds(PowerTime);
+        }
     }
     private void OnDrawGizmosSelected()
     {
